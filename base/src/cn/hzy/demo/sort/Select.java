b0027fa057cdf839ddd23db7e0c16dd11b72c5d7
huangzhuyun <huangzhuyun@haohandata.com.cn>
2021-03-18 20:05:07 +0800

    test

--- base/src/cn/hzy/demo/sort/Select.java
+++ base/src/cn/hzy/demo/sort/Select.java
@@ -1,107 +1,77 @@
 package cn.hzy.demo.sort;
 
+import cn.hzy.demo.sort.common.GenerateData;
+import cn.hzy.demo.sort.common.Sort;
+
 import java.security.Security;
 import java.util.Arrays;
 import java.util.Random;
 
-public class Select {
+public class Select extends Sort<Integer> {
 
-    public void selectDesc(int[] array){
-        for(int i=0;i<array.length-1;i++){
-            int index = i;
-            for(int j=index+1;j<array.length;j++){
-                if(array[j]>array[index]){
-                    index = j;
-                }
-            }
-            if(index!=i){
-                swap(array,index,i);
-            }
-        }
+    public Select() {
+        super("选择排序");
     }
 
-    public void selectAsc(int[] array){
-        for(int i=0;i<array.length-1;i++){
-            int index = i;
-            for(int j=index+1;j<array.length;j++){
-                if(array[j]<array[index]){
-                    index = j;
-                }
-            }
-            if(index!=i){
-                swap(array,index,i);
-            }
-        }
-    }
-
-    public void upgradeDesc(int[] array){
-        for(int i=0;i<array.length/2;i++){
-            int leftIndex = i;
-            int rightIndex = array.length-1-i;
-            int maxValueIndex = leftIndex;
-            int minValueIndex = rightIndex;
-            if(array[maxValueIndex] < array[minValueIndex]){
-                swap(array,maxValueIndex,minValueIndex);
-            }
-            for(int j=leftIndex+1;j<rightIndex;j++){
-                if(array[j]>array[maxValueIndex]){
-                    maxValueIndex = j;
-                }else if(array[j]<array[minValueIndex]){
-                    minValueIndex = j;
-                }
-            }
-            if(leftIndex!=maxValueIndex){
-                swap(array,leftIndex,maxValueIndex);
-            }
-            if(rightIndex!=minValueIndex){
-                swap(array,rightIndex,minValueIndex);
-            }
-        }
-    }
-
-    void print(int[] array){
-        for(int i:array){
-            System.out.print(i+",");
-        }
-        System.out.println();
-    }
-    int[] randomArray(int count){
-        Random random = new Random();
-        final int val = count*2;
-        int[] array = new int[count];
-        for(int i=0;i<count;i++){
-            array[i] = random.nextInt(val);
-        }
-        return array;
-    }
+//    public void upgradeDesc(int[] array){
+//        for(int i=0;i<array.length/2;i++){
+//            int leftIndex = i;
+//            int rightIndex = array.length-1-i;
+//            int maxValueIndex = leftIndex;
+//            int minValueIndex = rightIndex;
+//            if(array[maxValueIndex] < array[minValueIndex]){
+//                swap(array,maxValueIndex,minValueIndex);
+//            }
+//            for(int j=leftIndex+1;j<rightIndex;j++){
+//                if(array[j]>array[maxValueIndex]){
+//                    maxValueIndex = j;
+//                }else if(array[j]<array[minValueIndex]){
+//                    minValueIndex = j;
+//                }
+//            }
+//            if(leftIndex!=maxValueIndex){
+//                swap(array,leftIndex,maxValueIndex);
+//            }
+//            if(rightIndex!=minValueIndex){
+//                swap(array,rightIndex,minValueIndex);
+//            }
+//        }
+//    }
 
 
     public static void main(String[] args){
         Select select = new Select();
-        final int w = 10000;
-        int[] array = select.randomArray(20);
-        int[] other = Arrays.copyOf(array,array.length);
-        select.print(array);
-        select.print(other);
-
-        long start = System.currentTimeMillis();
-        select.upgradeDesc(other);
-        long end = System.currentTimeMillis();
-        System.out.print("cost"+ (end-start)/1000+" s");
-        select.print(other);
-
-        start = System.currentTimeMillis();
-        select.selectDesc(array);
-        end = System.currentTimeMillis();
-        System.out.print("cost"+ (end-start)/1000+" s");
-        select.print(array);
+        Integer[] array = GenerateData.asc(GenerateData.w1);
+        select.sortAsc(array);
     }
 
-    int count=0;
-    void swap(int[] array,int a,int b){
-        count++;
-        int temp = array[a];
-        array[a] = array[b];
-        array[b] = temp;
+    @Override
+    public void asc(Integer[] array) {
+        for(int i=0;i<array.length-1;i++){
+            int index = i;
+            for(int j=index+1;j<array.length;j++){
+                if(compareTo(j,index) == -1){
+                    index = j;
+                }
+            }
+            if(index!=i){
+                swap(index,i);
+            }
+        }
+    }
+
+    @Override
+    public void desc(Integer[] array) {
+        for(int i=0;i<array.length-1;i++){
+            int index = i;
+            for(int j=index+1;j<array.length;j++){
+                if(compareTo(j,index) == 1){
+                    index = j;
+                }
+            }
+            if(index!=i){
+                swap(index,i);
+            }
+        }
     }
 }
