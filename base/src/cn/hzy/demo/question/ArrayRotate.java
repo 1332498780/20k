51142446d1f473f3d7070b8df038aa3667156c00
hzy <15631166025@163.com>
2021-06-24 00:42:29 +0800

    test

--- base/src/cn/hzy/demo/question/ArrayRotate.java
+++ base/src/cn/hzy/demo/question/ArrayRotate.java
@@ -6,7 +6,11 @@ package cn.hzy.demo.question;
 public class ArrayRotate {
 
     public static void main (String[] args) {
-        int[] arr = {3,4,6,1,2};
+//        int[] arr = {3,4,6,1,2};
+//        int[] arr = {3};
+//        int[] arr = {1,1,1,0,1,1,1};
+        int[] arr = {1,2,3,4,5,6};
+
         System.out.println(arrayRotate1(arr));
     }
 
@@ -20,20 +24,31 @@ public class ArrayRotate {
     }
 
     public static int arrayRotate1(int[] array){
+        if(array == null || array.length ==0){
+            throw new NullPointerException();
+        }
         int i = 0,j = array.length-1;
-        while(i+1<j){
-            int mid = (j + i) >> 2;
+        while(array[i] >= array[j]){
+            int mid = (j + i) >> 1;
+            if(array[mid] == array[i] && array[mid] == array[j]){
+                //i,mid,j 都相等
+                return alternative(array);
+            }
             if(array[mid]>=array[i]){
                 //目标值在右边
                 i = mid;
             }else if(array[mid]<=array[j]){
                 j = mid;
             }
-            if(i==j+1){
+            if(i+1==j){
                 break;
             }
         }
-        return j;
+        return Math.min(array[i],array[j]);
+    }
+
+    private static int alternative(int[] array){
+        return arrayRotate(array);
     }
 
 }
